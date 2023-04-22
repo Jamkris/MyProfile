334a495719f63ea5225d90aab219ce48a3cc67fa
Jamkris <dltmgus1412@gmil.com>
2023-04-22 23:33:13 +0900

    MakeEasterEgg

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+function Error(){
+	const pass = prompt("Password");
+	if (pass == 9924) {
+		location.href="https://drive.google.com/drive/folders/1xIqvoxinORkp9aXYqBhrAqoCvLt1syT4?usp=sharing";
+	}
+	else {
+			alert("용케도 이스터에그를 찾았지만 비번이 있지 ㅋㅋ");
+	}
+};
