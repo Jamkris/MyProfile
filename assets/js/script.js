610a21a67ddfbb6b7309f8d16c130627cade5d7e
SeoungHyun <82251632+Jamkris@users.noreply.github.com>
2023-05-09 16:36:52 +0900

    PassWord

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,14 @@ function Error(){
 			alert("용케도 이스터에그를 찾았지만 비번이 있지 ㅋㅋ");
 	}
 };
+
+function PassHome() {
+	const pass = prompt("Password");
+	if (pass == 1234) {
+		location.href="https://drive.google.com/drive/folders/1OUuTZagsygaqKwS1329jdfCF0NnEccFV"
+	}
+	else {
+		alert("비밀번호가 틀렸습니다.")
+	}
+	
+}
